5c6de98b7e179436d5431d5574e496eed295d646
Alexander Evstratov <alexdrugev@me.com>
2024-03-30 02:54:07 +0300

    Finaly add working borders.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,11 +8,22 @@ public class PlayerController : MonoBehaviour
     public FixedJoystick fixedJoistick;
     public Rigidbody2D rb;
 
-    public void FixedUpdate()
-    {
-        //Vector3 direction = Vector3.forward * fixedJoistick.Vertical + Vector3.right * fixedJoistick.Horizontal;
-        //rb.AddForce(direction * speed * Time.fixedDeltaTime);
+    //public void FixedUpdate()
+    //{
+    //    Vector2 direction = (Vector2.up * fixedJoistick.Vertical + Vector2.right * fixedJoistick.Horizontal);
+    //    transform.Translate(direction * Time.deltaTime * _speed);
+    //}
 
-        transform.Translate((Vector2.up * fixedJoistick.Vertical + Vector2.right * fixedJoistick.Horizontal) * Time.deltaTime * _speed);
+    Vector2 MovementDirection; //Assuming this is assigned in Update() from Input
+    public float MaxSpeed;
+    void FixedUpdate()
+    {
+        //Vector2 finalMoveDir = MovementDirection.normalized * MaxSpeed;
+        Vector2 finalMoveDir = (Vector2.up * fixedJoistick.Vertical + Vector2.right * fixedJoistick.Horizontal);
+        //
+        //any additional changes to the final direction should happen here
+        //
+        finalMoveDir *= Time.deltaTime * _speed;
+        rb.MovePosition((Vector2)transform.position + finalMoveDir);
     }
 }

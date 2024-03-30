0ef0401d331ee6ff03b4740a21cc731557bbda96
Alexander Evstratov <alexdrugev@me.com>
2024-03-30 15:49:08 +0300

    Add player rotation.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private GameObject _lookAt;
+
     private float _speed = 5.0f;
     public FixedJoystick fixedJoistick;
     public Rigidbody2D rb;
+
+    private void Start()
+    {
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
 
     void FixedUpdate()
     {
@@ -12,5 +19,43 @@ public class PlayerController : MonoBehaviour
                                 Vector2.right * fixedJoistick.Horizontal);
         finalMoveDir *= Time.deltaTime * _speed;
         rb.MovePosition((Vector2)transform.position + finalMoveDir);
+
+        Extension.LookAt2D(transform, transform.up, (Vector2.up * fixedJoistick.Vertical +
+                                Vector2.right * fixedJoistick.Horizontal) * Time.deltaTime * _speed);
+
+
     }
+}
+
+static class Extension
+{
+    #region LookAt2D
+    public static void LookAt2D(this Transform me, Vector2 eye, Vector2 target)
+    {
+        Vector2 look = target - (Vector2)me.position;
+
+        float angle = Vector2.Angle(eye, look);
+
+        Vector2 right = Vector3.Cross(Vector3.forward, look);
+
+        int dir = 1;
+
+        if (Vector2.Angle(right, eye) < 90)
+        {
+            dir = -1;
+        }
+
+        me.rotation *= Quaternion.AngleAxis(angle * dir, Vector3.forward);
+    }
+
+    public static void LookAt2D(this Transform me, Vector2 eye, Transform target)
+    {
+        me.LookAt2D(eye, target.position);
+    }
+
+    public static void LookAt2D(this Transform me, Vector2 eye, GameObject target)
+    {
+        me.LookAt2D(eye, target.transform.position);
+    }
+    #endregion
 }
